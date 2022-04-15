f7d3b531bd12f8e35a3155f2628346784079694b
Nicolas Bize <nicolas.bize@subatech.in2p3.fr>
2022-04-15 12:23:19 +0200

    Add error bars on purity and efficiency plots

--- Detectors/GlobalTracking/include/GlobalTracking/MatchGlobalFwdAssessment.h
+++ Detectors/GlobalTracking/include/GlobalTracking/MatchGlobalFwdAssessment.h
@@ -140,8 +140,8 @@ class GloFwdAssessment
   std::unique_ptr<TH2D> mPairingEtaPt = nullptr;
   std::unique_ptr<TH2D> mTruePairingEtaPt = nullptr;
 
-  std::vector<std::unique_ptr<TH2D>> mPurityPtInnerVecTH2;
-  std::vector<std::unique_ptr<TH2D>> mPurityPtOuterVecTH2;
+  std::vector<std::unique_ptr<TEfficiency>> mPurityPtInnerVecTEff;
+  std::vector<std::unique_ptr<TEfficiency>> mPurityPtOuterVecTEff;
   std::vector<std::unique_ptr<TH1D>> mPairingPtInnerVecTH1;
   std::vector<std::unique_ptr<TH1D>> mPairingPtOuterVecTH1;
   std::vector<std::unique_ptr<TH1D>> mTruePairingPtInnerVecTH1;
